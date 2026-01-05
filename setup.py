a9bb65250bbfaffb2282ee68f2912dac1e5011a5
zapurza <vijaychaudhari0017@gmail.com>
2026-01-05 08:25:47 +0530

    Added Jupiter Notebook

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='This project will help reccomend the top 3 food items on swiggy homepage.',
+    description='This project will help recommend the top 3 food items on swiggy homepage.',
     author='Vijay Chaudhari',
     license='',
 )
